8c4648ccb955d62195e1e094e7aac6e06f4587a6
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-01-26 21:37:10 +0000

    Update 11-times-table.js

--- 11-times-table.js
+++ 11-times-table.js
@@ -1,36 +1,30 @@
 function sumNine() {
 var result = '11';
 var userInput = document.getElementById("num1").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerOne").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerOne").style.display = "none";
 }
 };
-
 function sumEight() {
 var result = '22';
 var userInput = document.getElementById("num2").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwo").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTwo").style.display = "none";
 }
 };
-
 function sumSeven() {
 var result = '33';
 var userInput = document.getElementById("num3").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerThree").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerThree").style.display = "none";
 }
 };
-
 function sumSix() {
 var result = '44';
 var userInput = document.getElementById("num4").value;
@@ -41,95 +35,78 @@ document.getElementById("tickcontainerFour").style.display = "flex";
 	document.getElementById("tickcontainerFour").style.display = "none";
 }
 };
-
 function sumFive() {
 var result = '55';
 var userInput = document.getElementById("num5").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFive").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerFive").style.display = "none";
 }
 };
-
 function sumFour() {
 var result = '66';
 var userInput = document.getElementById("num6").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSix").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerSix").style.display = "none";
 }
 };
-
 function sumThree() {
 var result = '77';
 var userInput = document.getElementById("num7").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSev").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerSev").style.display = "none";
 }
 };
-
 function sumTwo() {
 var result = '88';
 var userInput = document.getElementById("num8").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerEight").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerEight").style.display = "none";
 }
 };
-
 function sumOne() {
 var result = '99';
 var userInput = document.getElementById("num9").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerNine").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerNine").style.display = "none";
 }
 };
-
 function sumZero() {
 var result = '110';
 var userInput = document.getElementById("num10").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTen").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTen").style.display = "none";
 }
 };
-
 function sumEleven() {
 var result = '121';
 var userInput = document.getElementById("num11").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerEleven").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerEleven").style.display = "none";
 }
 };
-
 function sumTwelve() {
 var result = '132';
 var userInput = document.getElementById("num12").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwelve").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTwelve").style.display = "none";
 }
 };
-
 /*
 function printScreen() {
 	window.print();
