d518ceb45c0b5fb150b1d97ca6b3a13155f9c058
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-01-29 21:16:37 +0000

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,6 +1,5 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
