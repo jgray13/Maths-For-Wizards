e78f6fbc371a42b33728c34d0b5cad7e6d8708c6
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-01-29 22:37:19 +0000

    Update sum-from-twenty.js

--- sum-from-twenty.js
+++ sum-from-twenty.js
@@ -1,170 +1,138 @@
 function minusTwentyOne() {
 var result = '0';
 var userInput = document.getElementById("minus-twenty1").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerOneMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerOneMinus20").style.display = "none";
 }
 };
-
 function minusTwentyTwo() {
 var result = '1';
 var userInput = document.getElementById("minus-twenty2").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwoMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTwoMinus20").style.display = "none";
 }
 };
-
 function minusTwentyThree() {
 var result = '2';
 var userInput = document.getElementById("minus-twenty3").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerThreeMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerThreeMinus20").style.display = "none";
 }
 };
-
 function minusTwentyFour() {
 var result = '3';
 var userInput = document.getElementById("minus-twenty4").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFourMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerFourMinus20").style.display = "none";
 }
 };
-
 function minusTwentyFive() {
 var result = '4';
 var userInput = document.getElementById("minus-twenty5").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFiveMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerFiveMinus20").style.display = "none";
 }
 };
-
 function minusTwentySix() {
 var result = '5';
 var userInput = document.getElementById("minus-twenty6").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSixMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerSixMinus20").style.display = "none";
 }
 };
-
 function minusTwentySeven() {
 var result = '6';
 var userInput = document.getElementById("minus-twenty7").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSevMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerSevMinus20").style.display = "none";
 }
 };
-
 function minusTwentyEight() {
 var result = '7';
 var userInput = document.getElementById("minus-twenty8").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerEightMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerEightMinus20").style.display = "none";
 }
 };
-
 function minusTwentyNine() {
 var result = '8';
 var userInput = document.getElementById("minus-twenty9").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerNineMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerNineMinus20").style.display = "none";
 }
 };
-
 function minusTwentyTen() {
 var result = '9';
 var userInput = document.getElementById("minus-twenty10").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTenMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTenMinus20").style.display = "none";
 }
 };
-
-
-
 function minusTwentyEleven() {
 var result = '10';
 var userInput = document.getElementById("minus-twenty11").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerElevenMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerElevenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyTwelve() {
 var result = '11';
 var userInput = document.getElementById("minus-twenty12").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwelveMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTwelveMinus20").style.display = "none";
 }
 };
-
 function minusTwentyThirteen() {
 var result = '12';
 var userInput = document.getElementById("minus-twenty13").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerThirteenMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerThirteenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyFourteen() {
 var result = '13';
 var userInput = document.getElementById("minus-twenty14").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFourteenMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerFourteenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyOneFifteen() {
 var result = '14';
 var userInput = document.getElementById("minus-twenty15").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFifteenMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerFifteenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyOneSixteen() {
 var result = '15';
 var userInput = document.getElementById("minus-twenty16").value;
@@ -174,7 +142,6 @@ document.getElementById("tickcontainerSixteenMinus20").style.display = "flex";
 	document.getElementById("tickcontainerSixteenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyOneSeventeen() {
 var result = '16';
 var userInput = document.getElementById("minus-twenty17").value;
@@ -185,7 +152,6 @@ document.getElementById("tickcontainerSeventeenMinus20").style.display = "flex";
 	document.getElementById("tickcontainerSeventeenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyOneEighteen() {
 var result = '17';
 var userInput = document.getElementById("minus-twenty18").value;
@@ -196,7 +162,6 @@ document.getElementById("tickcontainerEighteenMinus20").style.display = "flex";
 	document.getElementById("tickcontainerEighteenMinus20").style.display = "none";
 }
 };
-
 function minusTwentyOneNineteen() {
 var result = '18';
 var userInput = document.getElementById("minus-twenty19").value;
@@ -207,22 +172,18 @@ document.getElementById("tickcontainerTwentyMinus19").style.display = "flex";
 	document.getElementById("tickcontainerTwentyMinus19").style.display = "none";
 }
 };
-
 function minusTwentyOneTwenty() {
 var result = '19';
 var userInput = document.getElementById("minus-twenty20").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwentyMinus20").style.display = "flex";
 } else {
 	document.getElementById("tickcontainerTwentyMinus20").style.display = "none";
 }
 };
-
 function minusTwentyOneTwentyOne() {
 var result = '20';
 var userInput = document.getElementById("minus-twenty21").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwentyoneMinus20").style.display = "flex";
 } else {
