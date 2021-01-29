4761a6040a867c30c5635944e80d544ec2591483
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-01-29 23:05:21 +0000

    Update sum-to-twenty.js

--- sum-to-twenty.js
+++ sum-to-twenty.js
@@ -1,170 +1,138 @@
 function sumNine() {
 var result = '19';
 var userInput = document.getElementById("num1").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerOne20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerOne20").style.display = "none";
 }
 };
-
 function sumEight() {
 var result = '18';
 var userInput = document.getElementById("num2").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwo20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerTwo20").style.display = "none";
 }
 };
-
 function sumSeven() {
 var result = '17';
 var userInput = document.getElementById("num3").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerThree20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerThree20").style.display = "none";
 }
 };
-
 function sumSix() {
 var result = '16';
 var userInput = document.getElementById("num4").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFour20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerFour20").style.display = "none";
 }
 };
-
 function sumFive() {
 var result = '15';
 var userInput = document.getElementById("num5").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFive20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerFive20").style.display = "none";
 }
 };
-
 function sumFour() {
 var result = '14';
 var userInput = document.getElementById("num6").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSix20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerSix20").style.display = "none";
 }
 };
-
 function sumThree() {
 var result = '13';
 var userInput = document.getElementById("num7").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSev20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerSev20").style.display = "none";
 }
 };
-
 function sumTwo() {
 var result = '12';
 var userInput = document.getElementById("num8").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerEight20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerEight20").style.display = "none";
 }
 };
-
 function sumOne() {
 var result = '11';
 var userInput = document.getElementById("num9").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerNine20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerNine20").style.display = "none";
 }
 };
-
 function sumZero() {
 var result = '10';
 var userInput = document.getElementById("num10").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerTen20").style.display = "none";
 }
 };
-
-
-
 function sumEleven() {
 var result = '9';
 var userInput = document.getElementById("num11").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerEleven20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerEleven20").style.display = "none";
 }
 };
-
 function sumTwelve() {
 var result = '8';
 var userInput = document.getElementById("num12").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwelve20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerTwelve20").style.display = "none";
 }
 };
-
 function sumThirteen() {
 var result = '7';
 var userInput = document.getElementById("num13").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerThirteen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerThirteen20").style.display = "none";
 }
 };
-
 function sumFourteen() {
 var result = '6';
 var userInput = document.getElementById("num14").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFourteen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerFourteen20").style.display = "none";
 }
 };
-
 function sumFifteen() {
 var result = '5';
 var userInput = document.getElementById("num15").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerFifteen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerFifteen20").style.display = "none";
 }
 };
-
 function sumSixteen() {
 var result = '4';
 var userInput = document.getElementById("num16").value;
@@ -174,44 +142,36 @@ document.getElementById("tickcontainerSixteen20").style.display = "flex";
 document.getElementById("tickcontainerSixteen20").style.display = "none";
 }
 };
-
 function sumSeventeen() {
 var result = '3';
 var userInput = document.getElementById("num17").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerSeventeen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerSeventeen20").style.display = "none";
 }
 };
-
 function sumEighteen() {
 var result = '2';
 var userInput = document.getElementById("num18").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerEighteen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerEighteen20").style.display = "none";
 }
 };
-
 function sumNineteen() {
 var result = '1';
 var userInput = document.getElementById("num19").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerNineteen20").style.display = "flex";
 } else {
 document.getElementById("tickcontainerNineteen20").style.display = "none";
 }
 };
-
 function sumTwenty() {
 var result = '0';
 var userInput = document.getElementById("num20").value;
-	
 if (userInput === result) {
 document.getElementById("tickcontainerTwenty20").style.display = "flex";
 } else {
