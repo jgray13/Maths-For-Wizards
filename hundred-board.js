ecf643eda505ffdc6c5d0f35c0f96617febc0123
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-04-13 13:08:05 +0100

    Add files via upload

--- hundred-board.js
+++ hundred-board.js
@@ -1,3 +1,7 @@
+// 2 event handlers for each input element
+// first event handler: add input to correctAnswers array if correct
+// second event listener: if user deletes/changes input, filter array to remove empty/incorrect inputs
+// final function for submit: remove duplicates (if user clicks enter after input, input re-entered)
 var correctAnswers = [];
 var hundredOne = document.getElementById("hundred-1");
 hundredOne.onkeyup = function() {
@@ -8,6 +12,15 @@ hundredOne.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredOne.addEventListener("keyup", function() {
+	var resultOne = "3";
+	if (hundredOne.value !== resultOne) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultOne;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredTwo = document.getElementById("hundred-2");
 hundredTwo.onkeyup = function() {
 	var inputTwo = hundredTwo.value;
@@ -17,6 +30,15 @@ hundredTwo.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredTwo.addEventListener("keyup", function() {
+	var resultTwo = "8";
+	if (hundredTwo.value !== resultTwo) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultTwo;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredThree = document.getElementById("hundred-3");
 hundredThree.onkeyup = function() {
 	var inputThree = hundredThree.value;
@@ -26,6 +48,15 @@ hundredThree.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredThree.addEventListener("keyup", function() {
+	var resultThree = "15";
+	if (hundredThree.value !== resultThree) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultThree;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredFour = document.getElementById("hundred-4");
 hundredFour.onkeyup = function() {
 	var inputFour = hundredFour.value;
@@ -35,6 +66,15 @@ hundredFour.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredFour.addEventListener("keyup", function() {
+	var resultFour = "17";
+	if (hundredFour.value !== resultFour) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultFour;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredFive = document.getElementById("hundred-5");
 hundredFive.onkeyup = function() {
 	var inputFive = hundredFive.value;
@@ -44,6 +84,15 @@ hundredFive.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredFive.addEventListener("keyup", function() {
+	var resultFive = "22";
+	if (hundredFive.value !== resultFive) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultFive;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredSix = document.getElementById("hundred-6");
 hundredSix.onkeyup = function() {
 	var inputSix = hundredSix.value;
@@ -53,6 +102,15 @@ hundredSix.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredSix.addEventListener("keyup", function() {
+	var resultSix = "30";
+	if (hundredSix.value !== resultSix) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultSix;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredSeven = document.getElementById("hundred-7");
 hundredSeven.onkeyup = function() {
 	var inputSeven = hundredSeven.value;
@@ -62,6 +120,15 @@ hundredSeven.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredSeven.addEventListener("keyup", function() {
+	var resultSeven = "33";
+	if (hundredSeven.value !== resultSeven) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultSeven;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredEight = document.getElementById("hundred-8");
 hundredEight.onkeyup = function() {
 	var inputEight = hundredEight.value;
@@ -71,6 +138,15 @@ hundredEight.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredEight.addEventListener("keyup", function() {
+	var resultEight = "39";
+	if (hundredEight.value !== resultEight) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultEight;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredNine = document.getElementById("hundred-9");
 hundredNine.onkeyup = function() {
 	var inputNine = hundredNine.value;
@@ -80,6 +156,15 @@ hundredNine.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredNine.addEventListener("keyup", function() {
+	var resultNine = "44";
+	if (hundredNine.value !== resultNine) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultNine;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredTen = document.getElementById("hundred-10");
 hundredTen.onkeyup = function() {
 	var inputTen = hundredTen.value;
@@ -89,6 +174,15 @@ hundredTen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredTen.addEventListener("keyup", function() {
+	var resultTen = "47";
+	if (hundredTen.value !== resultTen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultTen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredEleven = document.getElementById("hundred-11");
 hundredEleven.onkeyup = function() {
 	var inputEleven = hundredEleven.value;
@@ -98,6 +192,15 @@ hundredEleven.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredEleven.addEventListener("keyup", function() {
+	var resultEleven = "52";
+	if (hundredEleven.value !== resultEleven) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultEleven;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredTwelve = document.getElementById("hundred-12");
 hundredTwelve.onkeyup = function() {
 	var inputTwelve = hundredTwelve.value;
@@ -107,6 +210,15 @@ hundredTwelve.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredTwelve.addEventListener("keyup", function() {
+	var resultTwelve = "60";
+	if (hundredTwelve.value !== resultTwelve) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultTwelve;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredThirteen = document.getElementById("hundred-13");
 hundredThirteen.onkeyup = function() {
 	var inputThirteen = hundredThirteen.value;
@@ -116,6 +228,15 @@ hundredThirteen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredThirteen.addEventListener("keyup", function() {
+	var resultThirteen = "64";
+	if (hundredThirteen.value !== resultThirteen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultThirteen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredFourteen = document.getElementById("hundred-14");
 hundredFourteen.onkeyup = function() {
 	var inputFourteen = hundredFourteen.value;
@@ -125,6 +246,15 @@ hundredFourteen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredFourteen.addEventListener("keyup", function() {
+	var resultFourteen = "66";
+	if (hundredFourteen.value !== resultFourteen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultFourteen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredFifteen = document.getElementById("hundred-15");
 hundredFifteen.onkeyup = function() {
 	var inputFifteen = hundredFifteen.value;
@@ -134,6 +264,15 @@ hundredFifteen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredFifteen.addEventListener("keyup", function() {
+	var resultFifteen = "71";
+	if (hundredFifteen.value !== resultFifteen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultFifteen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredSixteen = document.getElementById("hundred-16");
 hundredSixteen.onkeyup = function() {
 	var inputSixteen = hundredSixteen.value;
@@ -143,6 +282,15 @@ hundredSixteen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredSixteen.addEventListener("keyup", function() {
+	var resultSixteen = "78";
+	if (hundredSixteen.value !== resultSixteen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultSixteen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredSeventeen = document.getElementById("hundred-17");
 hundredSeventeen.onkeyup = function() {
 	var inputSeventeen = hundredSeventeen.value;
@@ -152,6 +300,15 @@ hundredSeventeen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredSeventeen.addEventListener("keyup", function() {
+	var resultSeventeen = "86";
+	if (hundredSeventeen.value !== resultSeventeen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultSeventeen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredEighteen = document.getElementById("hundred-18");
 hundredEighteen.onkeyup = function() {
 	var inputEighteen = hundredEighteen.value;
@@ -161,6 +318,15 @@ hundredEighteen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredEighteen.addEventListener("keyup", function() {
+	var resultEighteen = "89";
+	if (hundredEighteen.value !== resultEighteen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultEighteen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredNineteen = document.getElementById("hundred-19");
 hundredNineteen.onkeyup = function() {
 	var inputNineteen = hundredNineteen.value;
@@ -170,6 +336,15 @@ hundredNineteen.onkeyup = function() {
 		console.log(correctAnswers);
 	}
 };
+hundredNineteen.addEventListener("keyup", function() {
+	var resultNineteen = "91";
+	if (hundredNineteen.value !== resultNineteen) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultNineteen;
+	    })
+		console.log(correctAnswers);
+	}
+});
 var hundredTwenty = document.getElementById("hundred-20");
 hundredTwenty.onkeyup = function() {
 	var inputTwenty = hundredTwenty.value;
@@ -177,8 +352,18 @@ hundredTwenty.onkeyup = function() {
 	if (inputTwenty === resultTwenty) {
 		correctAnswers.push(inputTwenty);
 		console.log(correctAnswers);
+		console.log(inputTwenty);
 	}
 };
+hundredTwenty.addEventListener("keyup", function() {
+	var resultTwenty = "95";
+	if (hundredTwenty.value !== resultTwenty) {
+	    correctAnswers = correctAnswers.filter(function(item) {
+		    return item !== resultTwenty;
+	    })
+		console.log(correctAnswers);
+	}
+});
 function hundredSub() {
 	var removeDuplicates = new Set(correctAnswers);
     console.log(removeDuplicates);
